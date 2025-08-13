ce22cb792dd2faf3fa3f09105d6e5743c63a28e8
AuY <thisisjohn0612@gmail.com>
2025-08-13 14:51:27 +0700

    Added Case sensitive check for shorten link

---
+++ URLShort.MVC/Helpers/QRGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using QRCoder;
+//using ZXing.QrCode.Internal;
+
+namespace URLShort.MVC.Helpers
+{
+    public static class GenerateQR
+    {
+        public static string FromUrl(string shortenedUrl)
+        {
+            // Create QR code
+            var qrGenerator = new QRCodeGenerator();
+            var qrCodeData = qrGenerator.CreateQrCode(shortenedUrl, QRCodeGenerator.ECCLevel.Q);
+            var qrCode = new QRCode(qrCodeData);
+            using var qrCodeImage = qrCode.GetGraphic(10);
+
+            // Convert to Base64 string
+            using var ms = new MemoryStream();
+            qrCodeImage.Save(ms, ImageFormat.Png);
+            var qrBase64 = Convert.ToBase64String(ms.ToArray());
+            
+            return $"data:image/png;base64,{qrBase64}";
+        }
+    }
+}
--- URLShort.MVC/Models/UrlShortener.cs
+++ URLShort.MVC/Models/UrlShortener.cs
@@ -1,16 +1,17 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿    using System.ComponentModel.DataAnnotations;
 
-namespace URLShort.MVC.Models
-{
-    public class ShortUrl
+    namespace URLShort.MVC.Models
     {
-        public int Id { get; set; }
-        public string? OriginalUrl { get; set; } = default!;
-        public string? ShortCode { get; set; } = default!;
+        public class ShortUrl
+        {
+            public int Id { get; set; }
+            public string? OriginalUrl { get; set; } = default!;
+            
+            public string? ShortCode { get; set; } = default!;
 
-        [MaxLength(8)]
-        public string RevokePassword { get; set; } = string.Empty;
+            [MaxLength(8)]
+            public string RevokePassword { get; set; } = string.Empty;
 
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        }
     }
-}
